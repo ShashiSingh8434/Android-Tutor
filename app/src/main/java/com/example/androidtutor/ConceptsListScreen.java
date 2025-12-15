de3d9cabf5810ea09ae7c289929d672e4816d92a
Shashi Singh <alphasingh8434@gmail.com>
2025-12-15 16:41:12 +0530

    Initial commit: Android Tutor project setup

---
+++ app/src/main/java/com/example/androidtutor/ConceptsListScreen.java
@@ -0,0 +1,49 @@
+package com.example.androidtutor;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ConceptsListScreen extends AppCompatActivity {
+
+    private boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_concepts_list_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (doubleBackToExitPressedOnce) {
+                    finish(); // exits app
+                    return;
+                }
+
+                doubleBackToExitPressedOnce = true;
+                Toast.makeText(ConceptsListScreen.this,
+                        "Press back again to exit",
+                        Toast.LENGTH_SHORT).show();
+
+                new Handler(Looper.getMainLooper()).postDelayed(() ->
+                        doubleBackToExitPressedOnce = false, 2000
+                );
+            }
+        });
+    }
+}
